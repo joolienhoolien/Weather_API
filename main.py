b42be6c8ac17bebfe655c71875d8198b28bc033e
joolienhoolien <julienpecquet@gmail.com>
2025-07-16 21:21:16 -0600

    updating some details about flask api documentation - hard coded right now

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from flask import Flask, render_template, request
 DEBUG = True
 
 
-app = Flask("Website")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>/")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
-
-app.run(debug=DEBUG)
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
