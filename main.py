c56fdca08a8d74804f80874d620b9c932b44a012
joolienhoolien <julienpecquet@gmail.com>
2025-07-18 19:01:35 -0600

    added yearly and all_data endpoints to api

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ def home():
     return render_template("home.html", data=stations.to_html())
 
 
+@app.route("/api/v1/<station>/")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    response = df.to_dict(orient="records")
+    return response
+
+
+@app.route("/api/v1/<station>/year/<year>")
+def year_data(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    response = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    #print(response)
+    return response
+
+
 @app.route("/api/v1/<station>/<date>/")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
